e0a4c5057f87ffbdf10f16ca208b521c0de26bbd
marco-oliveira7 <ma8155354@gmail.com>
2025-04-30 23:08:25 -0300

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:1000";
+  process.env.NODE_ENV === "production" ? undefined : "https://supermarketlist-server.onrender.com";
 
 export const socket = io(URL);
